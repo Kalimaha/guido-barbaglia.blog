8caa2ef5d22a3658a85315363d2a5f5431a445d9
Guido Barbaglia <guido@vinomofo.com>
2021-06-26 22:26:15 +1000

    WIP

--- src/markdown-pages/about.md
+++ src/markdown-pages/about.md
@@ -2,6 +2,7 @@
 slug: "/about"
 title: "About me"
 header: "Senior software engineer, expert in the analysis, design and development of open source web-based information systems. Strong experience in Web 2.0 technologies for the management, manipulation and dissemination of statistical data and micro-data. Excellent problem solving skills, enjoy working as a team member as well as independently."
+image: "about.jpeg"
 ---
 
 ## Work experience
@@ -11,14 +12,28 @@ header: "Senior software engineer, expert in the analysis, design and developmen
 | **Bold Key** | Value1 |
 | Normal Key   | Value2 |
 
+<br>
+
 ## Relevant projects
+
+<br>
 
 ## Education
 
+<br>
+
 ## Certfications
+
+<br>
 
 ## Technical skills
 
+<br>
+
 ## Publications
 
+<br>
+
 ## Honours and awards
+
+<br>
--- src/pages/{MarkdownRemark.frontmatter__slug}.js
+++ src/pages/{MarkdownRemark.frontmatter__slug}.js
@@ -1,29 +1,48 @@
 import React from "react"
 import { graphql } from "gatsby"
 import { Helmet } from "react-helmet";
-import { StaticImage } from "gatsby-plugin-image"
+// import { StaticImage } from "gatsby-plugin-image"
 
 export default function Template({
   data,
 }) {
-  const { markdownRemark } = data // data.markdownRemark holds your post data
-  const { frontmatter, html } = markdownRemark
+  const { markdownRemark } = data;
+  const { frontmatter, html } = markdownRemark;
   return (
-    <div className="blog-post-container" style={{ maxWidth: "1024px", marginLeft: "auto", marginRight: "auto", fontFamily: "Roboto" }}>
+    <div
+      className="container"
+      style={{ fontFamily: "Roboto", maxWidth: "1080px" }}
+    >
       <Helmet>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"></meta>
+        <meta name="format-detection" content="telephone=no"></meta>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge"></meta>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8"></meta>
+
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
         <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet" />
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"></link>
       </Helmet>
-      <div className="blog-post">
-        <StaticImage src="../images/avatar.jpeg" alt="Guido Barbaglia" width={200} height={200} />
-        <h1>{frontmatter.title}</h1>
-        <span>{frontmatter.header}</span>
-        <div
-          className="blog-post-content"
-          dangerouslySetInnerHTML={{ __html: html }}
-        />
+      <div style={{ minHeight: "1.5rem", height: "1.5rem" }}>
+        &nbsp;
       </div>
+      <div className="row">
+        <div className="col-lg-4">
+          <img
+            src={`${frontmatter.image}`}
+            className="img-fluid rounded-circle text-center"
+            alt={frontmatter.title}
+          />
+        </div>
+        <div className="col-lg-8">
+          <p style={{fontSize: "22px", fontWeight: 100}} className="text-justify">
+            {frontmatter.header}
+            </p>
+        </div>
+      </div>
+      <div dangerouslySetInnerHTML={{ __html: html }} />
     </div>
   )
 }
@@ -35,6 +54,7 @@ export const pageQuery = graphql`
         slug
         title
         header
+        image
       }
     }
   }
