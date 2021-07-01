e6d24fb07f722e6904e5cc89303b525d21e711d7
Guido Barbaglia <guido@vinomofo.com>
2021-07-01 15:56:21 +1000

    PreReact

--- gatsby-config.js
+++ gatsby-config.js
@@ -46,6 +46,7 @@ module.exports = {
     `gatsby-plugin-image`,
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
+    `gatsby-plugin-preact`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
