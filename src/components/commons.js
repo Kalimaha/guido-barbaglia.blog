1abac54ff3ee5b5ac9c7ad3ed980cb580404f9e7
Guido Barbaglia <guido@vinomofo.com>
2021-07-04 17:48:39 +1000

    Fix

--- src/components/commons.js
+++ src/components/commons.js
@@ -35,30 +35,26 @@ const ldJSON = (data) => ({
   }
 );
 
-export const Commons = (data) => {
-  console.log("==>", data)
-  return (
-    <Helmet htmlAttributes={{ lang: 'en' }}>
-      <title>Blog | Guido Barbaglia</title>
+export const Commons = (data) => (
+  <Helmet htmlAttributes={{ lang: 'en' }}>
+    <title>Blog | Guido Barbaglia</title>
 
+    <meta charset="utf-8" />
+    <meta name="description" content="Guido Barbaglia's personal blog."></meta>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=5.0"></meta>
+    <meta name="format-detection" content="telephone=no"></meta>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge"></meta>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"></meta>
 
-      <meta charset="utf-8" />
-      <meta name="description" content="Guido Barbaglia's personal blog."></meta>
-      <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=5.0"></meta>
-      <meta name="format-detection" content="telephone=no"></meta>
-      <meta http-equiv="X-UA-Compatible" content="IE=edge"></meta>
-      <meta http-equiv="Content-Type" content="text/html; charset=utf-8"></meta>
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"></link>
 
-      <link rel="preconnect" href="https://fonts.googleapis.com" />
-      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-      <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet" />
-      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"></link>
-
-      {data.data && data.data.markdownRemark ? (
-        <script type="application/ld+json">
-          {JSON.stringify(ldJSON(data.data))}
-        </script>
-      ) : (<script></script>)}
-    </Helmet>
-  )
-};
+    {data.data && data.data.markdownRemark ? (
+      <script type="application/ld+json">
+        {JSON.stringify(ldJSON(data.data))}
+      </script>
+    ) : (<script></script>)}
+  </Helmet>
+);
